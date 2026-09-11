167fe6062f3b05a8d2890ef3347eeed1332ec18a
Your Name <your_email@example.com>
2026-09-11 16:16:47 +0500

    Remove Supabase dependency to fix Vercel build exit code 1 error

--- api/index.py
+++ api/index.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route('/')
-def home():
-    return "FX Master is running!"
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run()
-    
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_video():
+    return render_template('upload.html')
+
+@app.route('/inbox')
+def inbox():
+    return render_template('inbox.html')
+
+@app.route('/friends')
+def friends():
+    return render_template('friends.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
