393d3d71bf4197d123447ce8535ab1c75036ac22
Your Name <your_email@example.com>
2026-09-14 10:36:22 +0500

    Fresh clean rebuild without any signal text and smooth upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,6 +22,5 @@ def inbox():
 def profile():
     return render_template('profile.html')
 
-# Vercel ke liye zaroori hai
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
