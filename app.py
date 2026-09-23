286e0e72aee1df5568f8012a16bca05e897cdf87
Your Name <your_email@example.com>
2026-09-23 15:50:52 +0500

    Configure 5 separate files for forex home bottom nav and separate pages

--- app.py
+++ app.py
@@ -6,25 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/market')
+def market():
+    return render_template('market.html')
+
 @app.route('/for-you')
 def for_you():
     return render_template('for_you.html')
 
-@app.route('/official-group')
-def official_group():
-    return render_template('official_group.html')
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
-
-@app.route('/inbox')
-def inbox():
-    return render_template('inbox.html')
-
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
+@app.route('/channel')
+def channel():
+    return render_template('channel.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
