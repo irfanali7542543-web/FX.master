e4c8a040285e560cee328d77784aec8211d48808
Your Name <your_email@example.com>
2026-09-23 12:07:55 +0500

    Create separate for_you.html template and fix 500 error

--- app.py
+++ app.py
@@ -3,32 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/market')
-def market():
-    return render_template('market.html')
-
-@app.route('/channel')
-def channel():
-    return render_template('channel.html')
-
 @app.route('/for-you')
-def for_you():
-    return render_template('new_features/for_you.html')
-
-@app.route('/inbox')
-def inbox():
-    return render_template('new_features/inbox.html')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
-
-@app.route('/upload')
-def upload():
-    return render_template('upload.html')
+for_you():
+    return render_template('for_you.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
